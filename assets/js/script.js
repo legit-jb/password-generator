77674c28cf1453d60599ad9efc1713c0b2a2d840
J Brennan <J_Brennan@me.com>
2021-03-08 23:46:49 -0500

    fixed typos

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,7 +84,7 @@ function writePassword() {
     }
 
     // now we have an array with the right length and charater types but we preselected some of the characters in a predictable way
-    // we will not randomive the order of the characters in the array using the Fisher Yates method
+    // we will now randomize the order of the characters in the array using the Fisher Yates method
     for (i = initialChars.length -1; i > 0; i--) {
       j = Math.floor(Math.random() * i);
       k = initialChars[i];
@@ -102,23 +102,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-// <!-- GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-// ``` -->
